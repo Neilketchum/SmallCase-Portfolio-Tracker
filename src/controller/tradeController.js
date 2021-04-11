07d36d214c2dc3df38d3ac1260f07a02cd7bb86f
Neilketchum <daipayanh@gmail.com>
2021-04-11 10:44:11 +0530

    Implemented Update Stocks Controller

--- src/controller/tradeController.js
+++ src/controller/tradeController.js
@@ -2,7 +2,16 @@ const Trades = require("../models/Trades")
 var chalk = require('chalk');
 var success = chalk.bold.green;
 var error = chalk.bold.red;
+const ObjectId = require('mongoose').Types.ObjectId;
+function isValidObjectId(id) {
 
+    if (ObjectId.isValid(id)) {
+        if ((String)(new ObjectId(id)) === id)
+            return true;
+        return false;
+    }
+    return false;
+}
 const getTotalShares = async (tickerSymbol) => {
     const trades = await Trades.find({ tickerSymbol });
     let count = 0;
@@ -62,34 +71,93 @@ exports.addTrade = async (req, res) => {
 
 exports.updateTrade = async (req, res) => {
     const tradeId = req.params['id'];
-    const trade = await Trades.findById(tradeId);
-    console.log(trade)
-    if (trade) {
-        const count = await getTotalShares(tickerSymbol);
+    if (isValidObjectId(tradeId)) {
+        const trade = await Trades.findById(tradeId);
+        console.log(trade)
+        if (trade) {
+            if (req.body.method === "BUY") {
+                Trades.findByIdAndUpdate(tradeId, {
+                    tickerSymbol: req.body.tickerSymbol,
+                    method: "BUY",
+                    price: req.body.price,
+                    shares: req.body.shares
+                }, (err, docs) => {
+                    if (err) {
+                        return res.status(400).send("Something Looks Wrong")
+                    } else {
+                        return res.status(200).send(docs)
+                    }
+                })
+            } else if (trade.method === "BUY" && req.body.method === "SELL") {
+                const count = await getTotalShares(req.body.tickerSymbol);
 
+                if ((count - trade.shares - req.body.shares) > 0) {
+                    Trades.findByIdAndUpdate(tradeId, {
+                        tickerSymbol: req.body.tickerSymbol,
+                        method: "SELL",
+                        price: req.body.price,
+                        shares: req.body.shares
+                    }, (err, docs) => {
+                        if (err) {
+                            return res.status(400).send("Something Looks Wrong")
+                        } else {
+                            return res.status(200).send(docs)
+                        }
+                    })
+                } else {
+                    return res.status(401).send("Cannot Update Trade as Updating this trade will result in selling more stocks than you own")
+                }
+            } else if (trade.method === "SELL" && req.body.method === "SELL") {
+                const count = await getTotalShares(req.body.tickerSymbol);
+                if ((count + trade.shares - req.body.shares) > 0) {
+                    Trades.findByIdAndUpdate(tradeId, {
+                        tickerSymbol: req.body.tickerSymbol,
+                        method: "SELL",
+                        price: req.body.price,
+                        shares: req.body.shares
+                    }, (err, docs) => {
+                        if (err) {
+                            return res.status(400).send("Something Looks Wrong")
+                        } else {
+                            return res.status(200).send(docs)
+                        }
+                    })
+                } else {
+                    return res.status(401).send("Cannot Update Trade as Updating this trade will result in selling more stocks than you own")
+                }
+            }
+
+        } else {
+            return res.status(401).send("No Trade with given ID exists");
+        }
     } else {
-        return res.status(401).send("No Trade with given ID exists");
+        return res.status(401).send("Request Parameters is not a Valid ID");
     }
+
 }
 exports.removeTrade = async (req, res) => {
     const tradeId = req.params['id'];
-    const trade = await Trades.findById(tradeId);
-    console.log(trade)
-    if (trade) {
-        if (trade.method === "BUY") {
-            const count = await getTotalShares(trade.tickerSymbol);
-            if (count > trade.shares) {
+    if (isValidObjectId(tradeId)) {
+        const trade = await Trades.findById(tradeId);
+        console.log(trade)
+        if (trade) {
+            if (trade.method === "BUY") {
+                const count = await getTotalShares(trade.tickerSymbol);
+                if (count > trade.shares) {
+                    const result = await Trades.findByIdAndDelete(tradeId)
+                    return res.status(200).send(result)
+                } else {
+                    return res.status(401).send("Cannot Delete Trade as deleting this trade will result in selling more stocks than you own")
+                }
+            }
+            if (trade.method === "SELL") {
                 const result = await Trades.findByIdAndDelete(tradeId)
                 return res.status(200).send(result)
-            } else {
-                return res.status(401).send("Cannot Delete Trade as deleting this trade will result in selling more stocks than you own")
             }
-        }
-        if (trade.method === "SELL") {
-            const result = await Trades.findByIdAndDelete(tradeId)
-            return res.status(200).send(result)
+        } else {
+            return res.status(401).send("No Trade with given ID exists");
         }
     } else {
-        return res.status(401).send("No Trade with given ID exists");
+        return res.status(401).send("Request Parameters is not a Valid ID");
     }
 }
